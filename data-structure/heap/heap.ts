67eef0cf4e58f317994aca1e3df72ecd7be6d13b
yangjingyu <yjy15680489038@163.com>
2021-06-08 16:33:30 +0800

    feat: change heap

--- data-structure/heap/heap.ts
+++ data-structure/heap/heap.ts
@@ -1,78 +1,3 @@
-class Heap{
-    type: "min" | "max"
-    value:number[]
-    constructor(type: "min" | "max" = "min") {
-        this.type = type
-        this.value = []
-    }
-
-    create() {
-        const length = this.value.length
-        for (let i = Math.floor((length / 2) - 1); i >= 0; i--){
-            this.ajust(i,length)
-        }
-    }
-
-    ajust(index:number,length:number) {
-        const array = this.value
-        for (let i = 2 * index + 1; i < length; i = 2 * i + 1){
-            if (i + 1 < length) {
-                if ((this.type === "max" && array[i + 1] > array[i]) || (this.type === "min" && array[i + 1] < array[i])) {
-                    i++
-                }
-            }
-            if ((this.type === "max" && array[index] < array[i]) || (this.type === "min" && array[index] > array[i])) {
-                [array[index],array[i]] = [array[i],array[index]]
-                index = i
-            } else {
-                break
-            }
-        }
-    }
-
-    add(num:number) {
-        const array = this.value
-        array.push(num)
-        if (array.length > 1) {
-            let index = array.length - 1
-            let target = Math.floor((index - 1) / 2)
-            while (target >= 0) {
-                if ((this.type === "min" && array[index] < array[target]) || (this.type === "max" && array[index] > array[target])) {
-                    [array[index],array[target]] = [array[target],array[index]]
-                    index = target
-                    target = Math.floor((index - 1) / 2)
-                } else {
-                    break
-                }
-            }
-        }
-    }
-
-    pop() {
-        const array = this.value
-        let result = null
-        if (array.length > 1) {
-            result = array[0]
-            array[0] = (array.pop() as number)
-            this.ajust(0,array.length)
-        } else if (array.length === 1) {
-            return array.pop()
-        }
-        return result
-    }
-}
-
-
-// var heap = new Heap('min');
-// [5, 6, 3, 11, 1, 0, 9].forEach(num => {
-//     heap.add(num)
-// })
-
-// console.log(heap.value);
-// console.log(heap.pop()); 
-// console.log(heap.value);
-
-
 class MyHeap{
     type: "min" | "max"
     heap:number[] //创建一个默认堆
@@ -82,18 +7,92 @@ class MyHeap{
     }
 
     //创建一个堆
-    createHeap(arr:number[]) {
-       //根据type判断是创建最大堆还是最小堆
-        
+    createHeap(arr: number[]) {
+        const len = arr.length
+        for (let i = Math.floor(len / 2) - 1; i >= 0; i--){
+            let index = i
+            for (let j = index * 2 + 1; j < len; j = j * 2 + 1){
+                //根据type判断是创建最大堆还是最小堆
+                if (this.type === "min") {
+                    if (j + 1 < len && arr[j + 1] < arr[j]) {
+                        j++
+                    }
+                    if (arr[index] <= arr[j]) {
+                        break
+                    } else {
+                        [arr[index], arr[j]] = [arr[j], arr[index]]
+                        index = j
+                    }
+                }
+                if (this.type === "max") {
+                    if (j + 1 < len && arr[j + 1] > arr[j]) {
+                        j++
+                    }
+                    if (arr[index] >= arr[j]) {
+                        break
+                    } else {
+                        [arr[index], arr[j]] = [arr[j], arr[index]]
+                        index = j
+                    }
+                }
+            }
+        }
+        this.heap = arr
     }
 
     //往堆中添加数据
     add(element: number) {
-        
+        const _arr = this.heap
+        _arr.push(element)
+        //重新排序
+        if (_arr.length === 1) return _arr
+        //最后一个与最底层的父节点进行相比
+        const len = _arr.length
+        let index = len - 1
+        let target = Math.floor(len / 2) - 1
+        while (target >= 0){
+            if (this.type === "min") {
+                if (_arr[target] > _arr[index]) {
+                    [_arr[target],_arr[index]] = [_arr[index],_arr[target]]
+                    index = target //获取交换位置后，插入的元素的element
+                    target = Math.floor((index - 1) / 2) //获取到更上一级的父节点，进行对比
+                } else {
+                    break
+                }
+            }
+            if (this.type === "max") {
+                if (_arr[target] < _arr[index]) {
+                    [_arr[target],_arr[index]] = [_arr[index],_arr[target]]
+                    index = target //获取交换位置后，插入的元素的element
+                    target = Math.floor((index - 1) / 2) //获取到更上一级的父节点，进行对比
+                } else {
+                    break
+                }
+            }
+        }
     }
 
     //获取数据
     pop() {
-        
+        const _arr = [...this.heap]
+        let result = null
+        if (_arr.length > 1) {
+            result = _arr[0]
+            _arr[0] = (_arr.pop() as number)
+            this.createHeap(_arr)
+        } else {
+            result = _arr.pop()
+        }
+        return result
     }
 }
+
+var heap1 = new MyHeap('max');
+heap1.createHeap([5, 6, 3, 11, 1, 0, 9])
+
+console.log(heap1.heap);
+heap1.add(12)
+console.log(heap1.heap);
+heap1.pop()
+console.log(heap1.heap);
+// console.log(heap.value);
--- dist/data-structure/heap/heap.js
+++ dist/data-structure/heap/heap.js
@@ -1,75 +1,11 @@
 "use strict";
-var Heap = /** @class */ (function () {
-    function Heap(type) {
-        if (type === void 0) { type = "min"; }
-        this.type = type;
-        this.value = [];
-    }
-    Heap.prototype.create = function () {
-        var length = this.value.length;
-        for (var i = Math.floor((length / 2) - 1); i >= 0; i--) {
-            this.ajust(i, length);
-        }
-    };
-    Heap.prototype.ajust = function (index, length) {
-        var _a;
-        var array = this.value;
-        for (var i = 2 * index + 1; i < length; i = 2 * i + 1) {
-            if (i + 1 < length) {
-                if ((this.type === "max" && array[i + 1] > array[i]) || (this.type === "min" && array[i + 1] < array[i])) {
-                    i++;
-                }
-            }
-            if ((this.type === "max" && array[index] < array[i]) || (this.type === "min" && array[index] > array[i])) {
-                _a = [array[i], array[index]], array[index] = _a[0], array[i] = _a[1];
-                index = i;
-            }
-            else {
-                break;
-            }
-        }
-    };
-    Heap.prototype.add = function (num) {
-        var _a;
-        var array = this.value;
-        array.push(num);
-        if (array.length > 1) {
-            var index = array.length - 1;
-            var target = Math.floor((index - 1) / 2);
-            while (target >= 0) {
-                if ((this.type === "min" && array[index] < array[target]) || (this.type === "max" && array[index] > array[target])) {
-                    _a = [array[target], array[index]], array[index] = _a[0], array[target] = _a[1];
-                    index = target;
-                    target = Math.floor((index - 1) / 2);
-                }
-                else {
-                    break;
-                }
-            }
-        }
-    };
-    Heap.prototype.pop = function () {
-        var array = this.value;
-        var result = null;
-        if (array.length > 1) {
-            result = array[0];
-            array[0] = array.pop();
-            this.ajust(0, array.length);
-        }
-        else if (array.length === 1) {
-            return array.pop();
-        }
-        return result;
-    };
-    return Heap;
-}());
-// var heap = new Heap('min');
-// [5, 6, 3, 11, 1, 0, 9].forEach(num => {
-//     heap.add(num)
-// })
-// console.log(heap.value);
-// console.log(heap.pop()); 
-// console.log(heap.value);
+var __spreadArrays = (this && this.__spreadArrays) || function () {
+    for (var s = 0, i = 0, il = arguments.length; i < il; i++) s += arguments[i].length;
+    for (var r = Array(s), k = 0, i = 0; i < il; i++)
+        for (var a = arguments[i], j = 0, jl = a.length; j < jl; j++, k++)
+            r[k] = a[j];
+    return r;
+};
 var MyHeap = /** @class */ (function () {
     function MyHeap(type) {
         if (type === void 0) { type = "min"; }
@@ -78,13 +14,96 @@ var MyHeap = /** @class */ (function () {
     }
     //创建一个堆
     MyHeap.prototype.createHeap = function (arr) {
-        //根据type判断是创建最大堆还是最小堆
+        var _a, _b;
+        var len = arr.length;
+        for (var i = Math.floor(len / 2) - 1; i >= 0; i--) {
+            var index = i;
+            for (var j = index * 2 + 1; j < len; j = j * 2 + 1) {
+                //根据type判断是创建最大堆还是最小堆
+                if (this.type === "min") {
+                    if (j + 1 < len && arr[j + 1] < arr[j]) {
+                        j++;
+                    }
+                    if (arr[index] <= arr[j]) {
+                        break;
+                    }
+                    else {
+                        _a = [arr[j], arr[index]], arr[index] = _a[0], arr[j] = _a[1];
+                        index = j;
+                    }
+                }
+                if (this.type === "max") {
+                    if (j + 1 < len && arr[j + 1] > arr[j]) {
+                        j++;
+                    }
+                    if (arr[index] >= arr[j]) {
+                        break;
+                    }
+                    else {
+                        _b = [arr[j], arr[index]], arr[index] = _b[0], arr[j] = _b[1];
+                        index = j;
+                    }
+                }
+            }
+        }
+        this.heap = arr;
     };
     //往堆中添加数据
     MyHeap.prototype.add = function (element) {
+        var _a, _b;
+        var _arr = this.heap;
+        _arr.push(element);
+        //重新排序
+        if (_arr.length === 1)
+            return _arr;
+        //最后一个与最底层的父节点进行相比
+        var len = _arr.length;
+        var index = len - 1;
+        var target = Math.floor(len / 2) - 1;
+        while (target >= 0) {
+            if (this.type === "min") {
+                if (_arr[target] > _arr[index]) {
+                    _a = [_arr[index], _arr[target]], _arr[target] = _a[0], _arr[index] = _a[1];
+                    index = target; //获取交换位置后，插入的元素的element
+                    target = Math.floor((index - 1) / 2); //获取到更上一级的父节点，进行对比
+                }
+                else {
+                    break;
+                }
+            }
+            if (this.type === "max") {
+                if (_arr[target] < _arr[index]) {
+                    _b = [_arr[index], _arr[target]], _arr[target] = _b[0], _arr[index] = _b[1];
+                    index = target; //获取交换位置后，插入的元素的element
+                    target = Math.floor((index - 1) / 2); //获取到更上一级的父节点，进行对比
+                }
+                else {
+                    break;
+                }
+            }
+        }
     };
     //获取数据
     MyHeap.prototype.pop = function () {
+        var _arr = __spreadArrays(this.heap);
+        var result = null;
+        if (_arr.length > 1) {
+            result = _arr[0];
+            _arr[0] = _arr.pop();
+            this.createHeap(_arr);
+        }
+        else {
+            result = _arr.pop();
+        }
+        return result;
     };
     return MyHeap;
 }());
+var heap1 = new MyHeap('max');
+heap1.createHeap([5, 6, 3, 11, 1, 0, 9]);
+console.log(heap1.heap);
+heap1.add(12);
+console.log(heap1.heap);
+heap1.pop();
+console.log(heap1.heap);
+// console.log(heap.value);
