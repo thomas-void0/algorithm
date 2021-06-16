6c0c203829926b5798f0fd1f17e685ca05022d81
yangjingyu <yjy15680489038@163.com>
2021-06-16 15:14:26 +0800

    upd: update getAfterOrder function

--- GetAfterOrder/index.ts
+++ GetAfterOrder/index.ts
@@ -15,3 +15,28 @@ XDEFAG
 BCA
 XEDGAF
  */
+namespace GetAfterOrder{
+    function getHRD(pre:any, vin:any):any {
+        if (!pre) {
+            return '';
+        }
+        if (pre.length === 1) {
+            return pre;
+        }
+        //获取根节点
+        const root = pre[0]
+        //获取根节点在中序遍历中的索引值
+        const index = vin.indexOf(root)
+        //获取中序遍历中的left和right
+        const vinLeft = vin.substring(0,index + 1)
+        const vinRight = vin.substring(index + 1)
+        //获取前序遍历中的left和right
+        const preLeft = pre.substring(1, index + 1)
+        const preRight = pre.substring(index + 1)
+        //返回遍历出的值
+        return getHRD(preLeft,vinLeft) + getHRD(preRight,vinRight) + root
+    }
+
+    console.log(getHRD("ABC","BAC")) 
+    console.log(getHRD("FDXEAG","XDEFAG")) 
+}
