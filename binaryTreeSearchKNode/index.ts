6d04533ef74bb443e4932c7cc4ea1e81a6bcca2f
yangjingyu <yjy15680489038@163.com>
2021-06-16 19:35:35 +0800

    feat: add binaryTreeSearchKNum function version.2

--- binaryTreeSearchKNode/index.ts
+++ binaryTreeSearchKNode/index.ts
@@ -34,12 +34,17 @@ namespace binaryTreeSearchKNode {
             }
         }
     };
-    
     const array:number[] = []
     function searchKNum(node:any,k:number) {
         //进行中序遍历
-        middleOrder(node);
-        return array[k - 1]
+
+        //递归
+        // middleOrder(node);
+        // return array[k -1]
+
+        //非递归
+        const arr = middleOrder2(node)
+        return arr[k - 1]
     }
 
     function middleOrder(node: any) {
@@ -49,6 +54,26 @@ namespace binaryTreeSearchKNode {
             middleOrder(node.right)
         }
         return []
+    }
+
+    //非递归实现
+    function middleOrder2(node: any) {
+        const result:number[] = []
+        const stack:any[] = []
+
+        let current = node
+        while (current || stack.length > 0) {
+            while (current) {
+                stack.push(current)
+                current = current.left
+            }
+
+            current = stack.pop()
+            result.push(current.data)
+            current = current.right
+        }
+        
+        return result
     }
 
     console.log("k1:",searchKNum(t1,3))
