2f77fa07ca0bbd4c16e55d79fc6632db13048dbf
yangjingyu <yjy15680489038@163.com>
2021-06-21 10:02:06 +0800

    fix: reverse

--- reverseChainTable/index.ts
+++ reverseChainTable/index.ts
@@ -1,19 +1,20 @@
 //反转一个链表：输入一个链表，反转链表后，输出新链表的表头。
 namespace reverseChainTable{
-    const chain3 = {val:2,next:null}
+    const chain3 = {val:3,next:null}
     const chain2 = {val:2,next:chain3}
     const chain1 = {val:1,next:chain2}
     
-    function reverseChainTable(chain: any) {
-        let temp = null
-        let current = chain
-        let originNext = chain.next
-        while (originNext) {
-            current.next = temp
-            temp = originNext
-            current = originNext
+    const reverseList = function (head:any) {
+        let currentNode = null;
+        let headNode = head;
+        while (head && head.next) {
+          currentNode = head.next; //chain2
+          head.next = currentNode.next; //chain3
+          currentNode.next = headNode;//chain1
+          headNode = currentNode;//chain2
         }
-    }
+        return headNode;
+    };
 
-   console.log(reverseChainTable(chain1))
+   console.log(reverseList(chain1))
 }
