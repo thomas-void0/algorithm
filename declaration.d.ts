4a3e166f0277afa9583c4bcfc53a262fae7b0ee4
yangjingyu <yjy15680489038@163.com>
2021-07-16 20:47:31 +0800

    feat: add d.ts

--- declaration.d.ts
+++ declaration.d.ts
@@ -17,3 +17,12 @@ declare class Node {
         this.children = []
     }
 }
+
+class ListNode {
+    val: number
+    next: ListNode | null
+    constructor(val?: number, next?: ListNode | null) {
+        this.val = (val === undefined ? 0 : val)
+        this.next = (next === undefined ? null : next)
+    }
+}
