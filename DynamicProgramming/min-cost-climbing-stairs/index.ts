57353a2ac510f5adcdcdeae987fc61d570d69344
yangjingyu <yjy15680489038@163.com>
2021-08-13 10:07:22 +0800

    feat: init

---
+++ DynamicProgramming/min-cost-climbing-stairs/index.ts
@@ -0,0 +1,11 @@
+/**
+ * 使用最小花费爬楼梯
+ * @see https://leetcode-cn.com/problems/min-cost-climbing-stairs/
+ */
+function minCostClimbingStairs(cost: number[]): number {
+    let result = 0
+
+
+    return result
+
+};
