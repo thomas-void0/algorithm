798d3dab91249c0ebc4473694000fb464a3546cb
thomas-void0 <yjy15680489038@163.com>
2022-03-25 09:55:06 +0800

    update: 更新快排

--- 排序/快速排序.js
+++ 排序/快速排序.js
@@ -1,31 +1,3 @@
-// 空间复杂度更低的快速排序
-var sortArray2 = function (nums) {
-  if (nums.length < 2) return nums;
-  return quickSort(nums, 0, nums.length - 1);
-};
-
-function quickSort(nums, left, right) {
-  if (left >= right) return;
-  let pivotIndex = partition(nums, left, right)
-  quickSort(nums, left, pivotIndex - 1)
-  quickSort(nums, pivotIndex + 1, right)
-  return nums;
-}
-
-function partition(nums, left, right) {
-  let pivot = right;
-  let leftIndex = left;
-  for (let i = left; i < right; i++) {
-    if (nums[i] < nums[pivot]) {
-      [nums[leftIndex], nums[i]] = [nums[i], nums[leftIndex]];
-      leftIndex++;
-    }
-  }
-  [nums[leftIndex], nums[pivot]] = [nums[pivot], nums[leftIndex]];
-  return leftIndex;
-}
-
-
 // 快速排序,
 function sortArray(nums) {
   if (nums.length <= 1) return nums
@@ -48,4 +20,3 @@ function sortArray(nums) {
   // 连接
   return sortArray(left).concat(pivot, sortArray(right))
 }
-
