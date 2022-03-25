ed74fe8582eb2874c318591f937e04a8f21af4d2
thomas-void0 <yjy15680489038@163.com>
2022-03-25 09:23:30 +0800

    update: 更新快速排序

--- 排序/快速排序.js
+++ 排序/快速排序.js
@@ -24,3 +24,28 @@ function partition(nums, left, right) {
   [nums[leftIndex], nums[pivot]] = [nums[pivot], nums[leftIndex]];
   return leftIndex;
 }
+
+
+// 快速排序,
+function sortArray(nums) {
+  if (nums.length <= 1) return nums
+
+  // 分区，找到基准值，拼接数组
+  const middle = Math.floor(nums / 2)
+  const pivot = nums.splice(middle, 1)[0]
+
+  const left = []
+  const right = []
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] <= pivot) {
+      left.push(nums[i])
+    } else {
+      right.push(nums[i])
+    }
+  }
+
+  // 连接
+  return sortArray(left).concat(pivot, sortArray(right))
+}
+
