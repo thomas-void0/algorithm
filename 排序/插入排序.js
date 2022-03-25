bf0314279dc5a3f7233fe7dc0df88838bf353452
thomas-void0 <yjy15680489038@163.com>
2022-03-25 09:38:32 +0800

    update: 复习插入排序

--- 排序/插入排序.js
+++ 排序/插入排序.js
@@ -1,10 +1,10 @@
 const arr = [1, 2, 0, 12, 5, 3]
-function sort(arr) {
-  for (let i = 1; i < arr.length; i++) {
-    // 将左边的区间认为已经排序完成
-    for (let j = i; i > 0; j--) {
-      if (arr[j] < arr[j - 1]) {
-        [arr[j], arr[j - 1]] = [arr[j - 1], arr[j]]
+// 插入排序：将左边的区间视为已经排序的区间。将右边没有排序的数与左边已经排好位置的数据进行对比。然后插入到左边
+function sortArray(nums) {
+  for (let i = 1; i < nums.length; i++) {
+    for (let j = i; j > 0; j--) {
+      if (nums[j] < nums[j - 1]) {
+        [nums[j], nums[j - 1]] = [nums[j - 1], nums[j]]
       } else {
         break
       }
