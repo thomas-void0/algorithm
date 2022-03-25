e91fccc2ec089f898abcc409e45ea10f1d8e2f99
thomas-void0 <yjy15680489038@163.com>
2022-03-25 09:31:06 +0800

    update: 复习选择排序

--- 排序/选择排序.js
+++ 排序/选择排序.js
@@ -1,16 +1,18 @@
 const arr = [1, 2, 0, 12, 5, 3]
-function sort(arr) {
-  for (let i = 0; i < arr.length; i++) {
-    let min = arr[i]
-    let idx = i
-    for (let j = i + 1; j < arr.length; j++) {
-      if (min > arr[j]) {
-        min = arr[j]
-        idx = j
+// 选择排序, 找到最小值。比它小的就放在数组的左侧。是一种原地排序算法
+function sortArray(nums) {
+  const len = nums.length
+  for (let i = 0; i < len; i++) {
+    let min = nums[i]
+    let minIdx = i
+    for (let j = 0; j < len; j++) {
+      if (nums[j] > min) {
+        min = nums[j]
+        minIdx = j
       }
     }
-    if (i !== idx) {
-      [arr[i], arr[idx]] = [arr[idx], arr[i]]
+    if (min !== nums[i]) {
+      [nums[i], nums[minIdx]] = [nums[minIdx], nums[i]]
     }
   }
 }
